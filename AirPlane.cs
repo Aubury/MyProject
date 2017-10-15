4287f0ecc7ef32cf3bfe268e267211a03fd9ed38
Tash <aubury@ukr.net>
2017-10-15 22:03:37 +0300

    Forsage

--- AirPlane.cs
+++ AirPlane.cs
@@ -1,12 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyProject
 {
     class AirPlane
     {
-    }
-}
+
+        public int Capacity { get; private set; }
+        public bool AutoPilotOn { get; set; }
+
+        /// <summary>
+        /// Fuel consuption. kg/km
+        /// </summary>
+        public float Consuption { get; private set; }
+
+        public int Altitude { get; private set; }
+
+
+        public static decimal TicketPrice { get; set; }
+        public static int MinAltitudeAuto { get; set; }
+        public static int MaxAltitudeAuto { get; set; }
+
+        private int _altitudeIncrement;
+        static AirPlane()
+        {
+            MinAltitudeAuto=2000;
+            MaxAltitudeAuto = 10000;
+        }
+        public AirPlane(int capacity, float consuption, int altitudeIncrement)
+        {
+            Altitude = 0;
+            AutoPilotOn = false;
+            Capacity = capacity;
+            Consuption = consuption;
+            _altitudeIncrement = altitudeIncrement;
+        }
+
+        public int Climb(int increment)
+        {
+            if (!AutoPilotOn) return Altitude += increment;
+
+            if (Altitude + increment < MaxAltitudeAuto)
+            {
+                return Altitude += increment;
+            }
+
+            return Altitude = MaxAltitudeAuto;
+        }
+
+        public int Down(int increment)
+        {
+            if (AutoPilotOn)
+            {
+                if (Altitude - increment > MinAltitudeAuto)
+                {
+                    return Altitude -= increment;
+                }
+                if (Altitude < MinAltitudeAuto) return Altitude;
+                //return Altitude = MinAltitudeAuto;
+            }
+
+            if (Altitude - increment < 0) return Altitude = 0;
+            return Altitude -= increment;
+        }
+
+        public int Forsage(int increment)
+        {
+            return increment * 2;
+        }
+        public void SetAltitude(int targetAlitude)
+        {
+            int speed = 100;
+
+            do
+              {
+                Climb(speed);
+                if (Altitude > MinAltitudeAuto) AutoPilotOn = true;
+                Console.WriteLine("Altitude = {0}, Autopilot = {1}", Altitude, AutoPilotOn);
+              }
+            while (Altitude <= targetAlitude);
+
+           do
+               {
+                  Down(speed);
+                  if (Altitude < MinAltitudeAuto) AutoPilotOn = false;
+                  Console.WriteLine("Altitude = {0}, Autopilot = {1}", Altitude, AutoPilotOn);
+                   }
+            while (Altitude>0);
+
+               }
+             }
+       
+       }
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Работа с классом Airplane.
+//Необходимо сделать:
+//- добавить статический конструктор.Устанавливать значения по умолчанию для свойств MinAltitudeAuto и MaxAltitudeAuto
+//- Реализовать метод SetAltitude.Он должен выводить самолет на заданную высоту.Текущая высота должна быть доступна в свойстве Altitude. 
+//Необходимо учесть ограничения, связанные с автопилотом - если он включен, высота не должна быть выше/ниже заданных пределов
+//- Реализовать свойство Forsage - при включенном форсаже инкремент подъема удваивается. Динамика снижения не меняется.
+//- Реализовать интерактивную работу с пользователем через командную строку: циклически запрашивать у пользователя желаемую высоту
+//- Пользователь из командной строки должен иметь возможность включать/выключать автопилот и форсаж.
+//- Разместить проект на собственном на GitHub и загрузить в logbook ссылку на репозиторий
+
+
+//Диалог с пользователем должен выглядеть примерно так:
+
+//> A= 11000 - пользователь ввел желаемую высоту
+//> Высота = 11000 - ответ приложения
+//> A= 7000 - пользователь ввел желаемую высоту
+//> Высота = 7000 - ответ приложения
+//> Auto = 1 - пользователь активировал автопилот
+//> Автопилот активирован                                                      - ответ приложения
+//> A= 11000 - пользователь ввел желаемую высоту
+//> Невозможно занять высоту 11000 в режиме автопилота.Текущая высота = 10000
+//> F = 1 - пользователь активировал форсаж
+//... и т.
+
+
+using System;
+
 
 namespace MyProject
 {
@@ -10,6 +32,25 @@ namespace MyProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("App started");
+
+            AirPlane.MaxAltitudeAuto = 10000;
+            AirPlane.MinAltitudeAuto = 2000;
+
+            AirPlane airplane = new AirPlane(140, 2.26F, 100);
+       
+            Console.WriteLine(airplane.Altitude);
+            airplane.SetAltitude(8000);
+
+            Console.WriteLine("Enter parameter: ");
+            do
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(input);
+                Console.WriteLine("Press any key");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.ReadLine();
         }
     }
 }
